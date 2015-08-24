4009261c601213bd72d8302a6e7060af7b6971bb
Mike Endale <mike@blencorp.com>
2015-08-24 18:31:14 -0400

    Updated the how it works section. 	modified:   app/_includes/footer.html 	new file:   app/_includes/how-it-works.html 	new file:   app/assets/images/apps/02-add-phone.png 	new file:   app/assets/images/apps/iphone-6.png 	modified:   app/assets/javascripts/app.js 	modified:   app/assets/stylesheets/app.scss 	modified:   app/assets/stylesheets/partials/_footer.scss 	new file:   app/assets/stylesheets/partials/_how-it-works.scss 	modified:   app/index.html

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -69,6 +69,19 @@ $(document).ready(function() {
   //     loop: true
   // });
 
+  // How it Works slider.
+  $(".bhoechie-tab-menu>div.list-group>a").click(function(e) {
+    e.preventDefault();
+    $(this).siblings('a.active').removeClass("active");
+    $(this).addClass("active");
+
+    var index = $(this).index();
+
+    $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+    $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+
+  });
+
 });
 
 // Start the show. This is exciting :-)
